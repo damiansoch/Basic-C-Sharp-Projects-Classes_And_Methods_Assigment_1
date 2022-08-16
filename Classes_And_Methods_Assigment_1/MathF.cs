45077a7657573bf7d501acef21994ee695542a54
damiansoch <66294165+damiansoch@users.noreply.github.com>
2022-08-16 08:55:04 +0100

    Add project files.

---
+++ Classes_And_Methods_Assigment_1/MathF.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_And_Methods_Assigment_1
+{
+    public class MathF
+    {
+        public static int findOposite(int number)
+        //the methid will take the user number and will return the opposite number
+        {
+            int opositeNumber = number * (-1);
+            return opositeNumber;
+        }
+
+        public static int findOposite(int numberOne, int numberTwo)
+        // the method will add two numbers and then return the oposite number
+        {
+            int opositeNumber = (numberOne + numberTwo) * (-1);
+            return opositeNumber;
+        }
+
+        public static int findOposite(string numberString)
+        //the methid will take the user number and will return the opposite number
+        {
+            int numberInt = Convert.ToInt32(numberString);
+            int opositeNumber = numberInt * (-1);
+            return opositeNumber;
+        }
+
+    }
+}
---
+++ Classes_And_Methods_Assigment_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_And_Methods_Assigment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = 10;
+            int numberTwo = -15;
+            string numberThree = "26";
+
+            int opositeNumberOne = MathF.findOposite(numberOne);
+            Console.WriteLine("The oposite number to the first number is {0}", opositeNumberOne);
+
+            int opositeNumberTwo = MathF.findOposite(numberTwo);
+            Console.WriteLine("The oposite number to the second number is {0}", opositeNumberTwo);
+
+            int opositeNumberSum = MathF.findOposite(numberOne, numberTwo);
+            Console.WriteLine("The oposite number to the sum of the two numbers is {0}", opositeNumberSum);
+
+            int opositeNumberThree = MathF.findOposite(numberThree);
+            Console.WriteLine("The oposite number to the third number (string) is {0}", opositeNumberThree);
+
+
+            Console.ReadLine();
+        }
+    }
+}
